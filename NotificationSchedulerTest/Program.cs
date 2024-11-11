ebf74e5a2ec75a5b8fe30b8865a923eb6de693a1
keith <keithmuwanguzi@gmail.com>
2024-11-11 14:23:17 +0300

    redo nnet8.0

---
+++ NotificationSchedulerTest/Program.cs
@@ -0,0 +1,22 @@
+﻿using NotificationScheduleLibrary;
+
+const string smtpHost = "smtp.gmail.com";
+const int smtpPort = 587;
+const string senderEmail = "jeysmuwanguzi@gmail.com";
+const string senderPassword = "tisk ulzh ddmc dyrm";
+
+var api = new LibraryApi(
+    smtpServer: smtpHost, 
+    smtpPort: smtpPort,                              
+    smtpUsername: senderEmail,      
+    smtpPassword: senderPassword,              
+    senderEmail: senderEmail   
+);
+            
+var reminderId = api.ScheduleMedication("Paracetamol", "100mg", DateTime.Now.AddSeconds(5), "Shanita","shanitadith@gmail.com");
+            
+Thread.Sleep(5000);
+            
+api.TriggerReminder(reminderId);
+            
+api.ShowLogs();
